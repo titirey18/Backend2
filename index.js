c8a666286a870faa1bde1c9a7cd79b57a636cf5c
Thiago Soubielle <152506649+titirey18@users.noreply.github.com>
2025-03-06 11:05:20 -0300

    commit modificacion

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app = express()
 
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: "*", 
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
 app.use(express.json())
 
